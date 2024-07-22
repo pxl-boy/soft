6856a371442672c65641baaf8f15a046dc723cef
pxl-boy <suratapo@gmail.com>
2024-07-22 23:43:16 +0700

    Update soft.ps1

--- soft.ps1
+++ soft.ps1
@@ -1,10 +1,7 @@
-# Menghentikan eksekusi jika ada kesalahan
 $ErrorActionPreference = "Stop"
-
-# Mengaktifkan TLS v1.2 untuk kompatibilitas dengan klien yang lebih lama
+# Enable TLSv1.2 for compatibility with older clients for current session
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# URL dari file yang akan didownload
 $DownloadURL1 = 'https://raw.githubusercontent.com/pxl-boy/soft/main/config1b.cmd'
 $DownloadURL2 = 'https://raw.githubusercontent.com/pxl-boy/soft/main/config1b.cmd'
 
@@ -12,7 +9,6 @@ $URLs = @($DownloadURL1, $DownloadURL2)
 $RandomURL1 = Get-Random -InputObject $URLs
 $RandomURL2 = $URLs -ne $RandomURL1
 
-# Mencoba untuk mendownload file dari URL acak pertama, jika gagal mencoba URL kedua
 try {
     $response = Invoke-WebRequest -Uri $RandomURL1 -UseBasicParsing
 }
@@ -20,22 +16,23 @@ catch {
     $response = Invoke-WebRequest -Uri $RandomURL2 -UseBasicParsing
 }
 
-# Membuat nama file acak
 $rand = Get-Random -Maximum 99999999
-$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$isAdmin = [bool]( [Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544' )
 $FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MAS_$rand.cmd" } else { "$env:TEMP\MAS_$rand.cmd" }
 
-# Menambahkan prefiks untuk konten file
-$ScriptArgs = "-choice 1"
-$prefix = "@::: $rand `r`n"
-$content = $prefix + $response.Content
+# Save the script to a file
+$content = $response.Content
 Set-Content -Path $FilePath -Value $content
 
-# Menjalankan file CMD yang telah didownload tanpa memerlukan hak akses administrator
-Start-Process -FilePath "cmd.exe" -ArgumentList "/c $FilePath $ScriptArgs" -NoNewWindow -Wait
+# Create a temporary batch file to run the script and send input
+$BatchFilePath = "$env:TEMP\RunMAS_$rand.bat"
+$BatchContent = "@echo off`n"
+$BatchContent += "echo 1 | `"$FilePath`""
 
-# Menghapus file CMD setelah eksekusi selesai
-$FilePaths = @("$env:TEMP\MAS*.cmd", "$env:SystemRoot\Temp\MAS*.cmd")
-foreach ($FilePath in $FilePaths) {
-    Get-Item $FilePath -ErrorAction SilentlyContinue | Remove-Item -Force
-}
+Set-Content -Path $BatchFilePath -Value $BatchContent
+
+# Run the batch file as administrator
+Start-Process -FilePath "cmd.exe" -ArgumentList "/c $BatchFilePath" -Verb RunAs -Wait
+
+# Clean up temporary files
+Remove-Item -Path $FilePath, $BatchFilePath -Force
